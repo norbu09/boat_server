1712c635fff316dcdaea7d826ae6d69014df7a7d
Lenz Gschwendtner <lenz@springtimesoft.com>
2020-02-04 18:12:33 +0000

    added a few more metrics

--- lib/boat_server/prometheus.ex
+++ lib/boat_server/prometheus.ex
@@ -1,5 +1,6 @@
 defmodule BoatServer.Prometheus do
   use Prometheus.Metric
+  require Logger
 
   def setup do
     Gauge.new(
@@ -19,18 +20,52 @@ defmodule BoatServer.Prometheus do
       labels: [:boat, :source],
       help: "Apparent wind angle in degrees"
     )
+
+    Gauge.new(
+      name: :position_latitude,
+      labels: [:boat, :source],
+      help: "Latitude of current position"
+    )
+
+    Gauge.new(
+      name: :position_longitude,
+      labels: [:boat, :source],
+      help: "Longitude of current position"
+    )
+
+    Gauge.new(
+      name: :position_altitude,
+      labels: [:boat, :source],
+      help: "Altitude of current position"
+    )
+
+    Gauge.new(
+      name: :course_over_ground_true,
+      labels: [:boat, :source],
+      help: "Current true course over ground"
+    )
+
+    Gauge.new(
+      name: :speed_over_ground,
+      labels: [:boat, :source],
+      help: "Current speed over ground"
+    )
   end
 
-  def instrument(%{"environment" => env, "name" => name}) do
-    speed = convert(:ms_to_kn, env["wind"]["speedApparent"]["value"])
-    deg = convert(:rad_to_degree, env["wind"]["angleApparent"]["value"])
-    Gauge.set([name: :wind_speed_apparent, labels: [name, "signalk"]], speed)
-    Gauge.set([name: :wind_angle_apparent_deg, labels: [name, "signalk"]], deg)
+  def instrument(%{"navigation" => nav, "environment" => env, "name" => name}) do
+    wind_speed = convert(:ms_to_kn, env["wind"]["speedApparent"]["value"])
+    wind_deg = convert(:rad_to_degree, env["wind"]["angleApparent"]["value"])
+    course = convert(:rad_to_degree, nav["courseOverGroundTrue"]["value"])
+    boat_speed = convert(:ms_to_kn, nav["speedOverGround"]["value"])
 
-    Gauge.set(
-      [name: :wind_angle_apparent, labels: [name, "signalk"]],
-      env["wind"]["angleApparent"]["value"]
-    )
+    Gauge.set([name: :wind_speed_apparent, labels: [name, "signalk"]], wind_speed)
+    Gauge.set([name: :wind_angle_apparent_deg, labels: [name, "signalk"]], wind_deg)
+    Gauge.set([name: :wind_angle_apparent, labels: [name, "signalk"]], env["wind"]["angleApparent"]["value"])
+    Gauge.set([name: :position_longitude, labels: [name, "signalk"]], nav["position"]["longitude"])
+    Gauge.set([name: :position_latitude, labels: [name, "signalk"]], nav["position"]["latitude"])
+    Gauge.set([name: :position_altitude, labels: [name, "signalk"]], nav["position"]["altitude"])
+    Gauge.set([name: :course_over_ground_true, labels: [name, "signalk"]], course)
+    Gauge.set([name: :speed_over_ground, labels: [name, "signalk"]], boat_speed)
 
   end
 
